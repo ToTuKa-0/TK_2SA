76c009f6398f27955bac23192afd72bef5078631
ToTuKa <Kt08007921@gmail.com>
2025-08-20 16:07:02 +0900

    ジャンプ追加

--- Assets/Use/Script/PlayerMove.cs
+++ Assets/Use/Script/PlayerMove.cs
@@ -7,8 +7,12 @@ public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb; //Rigidbody2D参照
     private SpriteRenderer sr; //SpriteRenderer参照
-    public int jump; 
+
+    private int jump_f = 1; //ジャンプの初速
+    private float jump_m = 0.5f; //ボタンを押せる長さ
+    private int jump_p = 5; //ジャンプ中に加わる力
     bool jumping; //true(空中)かfolse(地面)しか受け付けない
+    private float jump_t; //残りジャンプ可能時間
 
     [HideInInspector] public float speed; //Xの移動速度
     [HideInInspector] public bool rightFacing; //向いている方向(true右 false左)
@@ -16,8 +20,8 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         //参照コンポーネント取得
-        rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>(); //Rigidbody2Dを取得
+        sr = GetComponent<SpriteRenderer>(); //SpriteRendererを取得
 
         rightFacing = true; // 最初は右向き
     }
@@ -27,10 +31,30 @@ public class PlayerMove : MonoBehaviour
         MoveUpdate(); //左右移動
 
         //ジャンプ
-        if (Input.GetKeyDown(KeyCode.Space) && !jumping) //スペースを押されたか+まだ地面にいるか
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) //スペースを押されたか+まだ地面にいるか
         {
-            rb.AddForce(new Vector2(0, jump));
-            jumping = true;
+            jumping = true; //ジャンプ中のフラグ
+            jump_t = jump_m; //経過時間リセット
+            rb.velocity = new Vector2(rb.velocity.x, jump_f); //上方向にジャンプの初速を加える
+        }
+
+        //ボタンを押している間に追加の力を加える
+        if (Input.GetKey(KeyCode.Space) && jumping)
+        {
+            if (jump_t > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump_p); //ジャンプ中に時間が残っている場合は力が加算される
+                jump_t -= Time.deltaTime; //ジャンプ可能時間を減らす
+            }
+            else
+            {
+                jumping = false; //時間経過で終了
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumping = false;
         }
     }
 
@@ -71,5 +95,10 @@ public class PlayerMove : MonoBehaviour
         {
             jumping = false;
         }
+    }
+
+    bool IsGrounded() //地面にいるかどうか判定
+    {
+        return Mathf.Abs(rb.velocity.y) < 0.01f; //y方向の速度がほぼゼロなら地面にいるとみなす
     }
 }

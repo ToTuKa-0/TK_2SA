3650891fea8949f7dd7c26c2c0a8e5ae5f5ab229
ToTuKa <Kt08007921@gmail.com>
2025-08-20 13:12:01 +0900

    変更2

--- Assets/Use/Script/PlayerMove.cs
+++ Assets/Use/Script/PlayerMove.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb; //Rigidbody2D参照
-    private int speed; //スピード変数
+    private SpriteRenderer sr; //SpriteRenderer参照
+    public int jump; 
+    bool jumping; //true(空中)かfolse(地面)しか受け付けない
+
+    [HideInInspector] public float speed; //Xの移動速度
+    [HideInInspector] public bool rightFacing; //向いている方向(true右 false左)
 
     void Start()
     {
+        //参照コンポーネント取得
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        rightFacing = true; // 最初は右向き
     }
 
     void Update()
     {
         MoveUpdate(); //左右移動
+
+        //ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping) //スペースを押されたか+まだ地面にいるか
+        {
+            rb.AddForce(new Vector2(0, jump));
+            jumping = true;
+        }
     }
 
     void MoveUpdate()
@@ -24,10 +40,16 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) //左移動
         {
             speed = -6; //移動速度
+
+            rightFacing = false; //右向きOFF
+            sr.flipX = true; //反転した向き
         }
         else if (Input.GetKey(KeyCode.D)) //右移動
         {
             speed = 6; //移動速度
+
+            rightFacing = true; //右向きON
+            sr.flipX = false; //通常の向き
         }
         else //入力無し
         {
@@ -41,5 +63,13 @@ public class PlayerMove : MonoBehaviour
         Vector2 velocity = rb.velocity; //移動速度を現在値から取得
         velocity.x = speed; //X方向の速度を入力から決定
         rb.velocity = velocity; //計算した移動速度をRigidbody2Dに反映
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) //Grounfと衝突した時の判定
+        {
+            jumping = false;
+        }
     }
 }
